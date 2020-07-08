45243675d65715aae3f3e226f6666507978d2f4f
zhuyanxi <547110356@qq.com>
2020-07-08 15:57:39 +0800

    Update strings.go

--- strings.go
+++ strings.go
@@ -5,3 +5,13 @@ func DeduplicateArray(s []string) []string {
 	res := NewSet(s...)
 	return res.Slice()
 }
+
+// ContainsString : return first index of val in array, if val is not in array return -1
+func ContainsString(array []string, val string) int {
+	for idx, item := range array {
+		if item == val {
+			return idx
+		}
+	}
+	return -1
+}

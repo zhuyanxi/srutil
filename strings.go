92adb21d3bca882123572d31842545c0ea062646
yan-xi.zhu <547110356@qq.com>
2021-12-10 17:18:13 +0800

    Update strings.go

--- strings.go
+++ strings.go
@@ -1,5 +1,10 @@
 package srutil
 
+import (
+	"reflect"
+	"unsafe"
+)
+
 // DeduplicateArray : depulicate a slice
 func DeduplicateArray(s []string) []string {
 	res := NewSet(s...)
@@ -14,4 +19,17 @@ func ContainsString(array []string, val string) int {
 		}
 	}
 	return -1
+}
+
+func Str2Bytes(s string) (b []byte) {
+	h1 := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	h2 := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	h2.Cap = h1.Len
+	h2.Len = h1.Len
+	h2.Data = h1.Data
+	return
+}
+
+func Bytes2Str(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
